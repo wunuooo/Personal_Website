85547227f596d0dc82d1bfdd28e4d1ca05fed6f0
wunuoo <xuwunuo@foxmail.com>
2024-12-08 01:23:26 +0800

    20241208v2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,8 +6,8 @@ const Contact = () => {
     return (
         <div className="container mx-auto my-8 p-4 main_content">
             <h1 className="text-3xl font-bold mb-4">联系方式</h1>
-            <p>既然你看到了这，一定访问过了我的微信公众号了。</p>
-            <p>其它联系方式后续添加。</p>
+            <p>既然你打开了这个网站，那么大概率访问过了我的微信公众号了。</p>
+            <p>其它联系方式会在后续添加。</p>
         </div>
     );
 };
